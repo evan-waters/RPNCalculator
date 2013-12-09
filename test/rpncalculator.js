3e68a004c4f0afd6ac20a985280571569a3cff93
Evan Waters <wakeboarder1335@gmail.com>
2013-12-08 19:14:41 -0700

    return false if too few arguments passed

--- test/rpncalculator.js
+++ test/rpncalculator.js
@@ -4,7 +4,9 @@ var rpncalculator = require('..')
   , should = require('should');
 
 describe('rpncalculator', function() {
+  
   describe('.calculate', function() {
+    
     describe('with invalid input (not numbers or accepted operators)', function() {
       it('should return an error', function(done) {
         rpncalculator.calculate(['4-'], function(err, result) {
@@ -12,7 +14,30 @@ describe('rpncalculator', function() {
           should.not.exist(result);
           done();
         });
-      })
-    })
+      });
+    });
+
+    describe('with too few arguments', function() {
+      it('should return false', function(done) {
+        var expected = 3;
+
+        function checkDone() {
+          expected--;
+          if (expected === 0) {
+            done();
+          }
+        }
+
+        var test = function(err, result) {
+          should.not.exist(err);
+          result.should.be.false;
+          checkDone();
+        };
+
+        rpncalculator.calculate([], test);
+        rpncalculator.calculate([1],test);
+        rpncalculator.calculate([1,2], test);
+      });
+    });
   });
 });
